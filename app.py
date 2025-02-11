91e754f0425fb86187868ed9c01294f129b01fa4
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-02-11 15:48:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from streamlit_folium import folium_static
 st.set_page_config(layout="wide")
 
 # Título de la aplicación
-st.title("Mapas de Fiebre Amarilla 2025")
+st.title("Mapas de Fiebre Amarilla 2025 🦟")
 
 # Instrucciones
 st.write("Este es un mapa interactivo de ejemplo con Streamlit y Folium.")
