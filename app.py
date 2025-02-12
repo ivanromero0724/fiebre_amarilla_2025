5952d527c4ecbeadf9a90aef5be5d02a15022c41
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-02-11 23:05:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ with col3:
 col1, col2, col3 = st.columns(3)  # Ajusta los valores según el ancho deseado
 
 with col2:  # Coloca la tabla en la columna central
-    st.markdown("<h6 style='text-align: center;'>Resumen de Viviendas por Municipio</h6>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center; font-weight: bold;'>Resumen de Viviendas por Municipio</h6>", unsafe_allow_html=True)
 
     tabla_resumen = datos.groupby("1_MUNICIPIO")["6_VIVIENDA_EFECTIVA_"].value_counts().unstack(fill_value=0)
     tabla_resumen["Total"] = tabla_resumen.sum(axis=1)
