0de61b6c4bbc7f830a542486f7caecbaa4d9e4a6
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-02-11 20:37:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ st.markdown("""
     <style>
         .legend-container {
             position: absolute;
-            bottom: 0px;
+            bottom: -30px;
             left: 10px;
             background-color: white;
             padding: 10px;
