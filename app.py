78eef26c926deccaddf52cbaa477de13f23f7c38
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-02-11 23:51:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -135,12 +135,12 @@ st.markdown("""
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    fig1 = px.pie(datos, names="1_MUNICIPIO", title="Distribución por Municipio",color_discrete_sequence=px.colors.qualitative.Safe)
+    fig1 = px.pie(datos, names="1_MUNICIPIO", title="Distribución de viviendas por Municipio",color_discrete_sequence=px.colors.qualitative.Safe)
     fig1.update_layout(title={'x': 0.5, 'xanchor': 'center'})
     st.plotly_chart(fig1, use_container_width=True)
 
 with col2:
-    fig2 = px.pie(datos, names="2_AREA", title="Distribución por Área",color_discrete_sequence=px.colors.qualitative.Safe)
+    fig2 = px.pie(datos, names="2_AREA", title="Distribución de viviendas por Área",color_discrete_sequence=px.colors.qualitative.Safe)
     fig2.update_layout(title={'x': 0.5, 'xanchor': 'center'})
     st.plotly_chart(fig2, use_container_width=True)
 
