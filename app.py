2399e8c0c477fa8d2d4c60ecbd3d3352d5d2b213
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-02-12 20:18:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ col1, col2, col3= st.columns(3)  # Ajusta los valores según el ancho deseado
 
 with col2:  # Coloca la tabla en la columna central
     st.markdown(
-    "<div style='margin-top: 40px;'></div>", 
+    "<div style='margin-top: 38px;'></div>", 
     unsafe_allow_html=True
 )
     st.markdown("<h6 style='text-align:center; font-weight: bold;'>Resumen de Viviendas por Municipio</h6>", unsafe_allow_html=True)
