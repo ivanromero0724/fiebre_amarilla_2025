9cf61113b8b6f40276ebdc3828ff72b8e5c6d573
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-02-14 16:40:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -227,11 +227,11 @@ with col3:  # Coloca la tabla en la columna central
     st.dataframe(tabla_resumen.style.set_properties(**{'background-color': 'white', 'border-radius': '10px', 'padding': '10px'}))
 
 with col1:
-    fig4 = px.pie(fa_datos, names="nmun_proce", title="Distribución de Casos de FA por Municipio",color_discrete_sequence=px.colors.qualitative.Safe)
+    fig4 = px.pie(fa_datos, names="nmun_proce", title="Distribución de Casos Confirmados de FA por Municipio",color_discrete_sequence=px.colors.qualitative.Safe)
     fig4.update_layout(title={'x': 0.5, 'xanchor': 'center'})
     st.plotly_chart(fig4, use_container_width=True)
 
 with col2:
-    fig5 = px.pie(fa_datos, names="Estado del caso", title="Estado de los Casos Confirmados de Fiebre Amarilla",color_discrete_sequence=px.colors.qualitative.Safe)
+    fig5 = px.pie(fa_datos, names="Estado del caso", title="Estado de los Casos Confirmados de FA",color_discrete_sequence=px.colors.qualitative.Safe)
     fig5.update_layout(title={'x': 0.5, 'xanchor': 'center'})
     st.plotly_chart(fig5, use_container_width=True)
