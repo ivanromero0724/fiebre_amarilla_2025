b4756ce13c65f2485aaba0ced16aedcad18aff0f
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-02-16 22:57:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ st.markdown(
 
 
 # URL del archivo en GitHub
-url = "https://raw.githubusercontent.com/ivanromero0724/fiebre_amarilla_2025/main/2025-02-15_2.xlsx"
+url = "https://raw.githubusercontent.com/ivanromero0724/fiebre_amarilla_2025/main/2025-02-16.xlsx"
 # Cargar los datos desde el archivo Excel
 df = pd.read_excel(url, engine="openpyxl")
 # Filtrar valores nulos en las columnas necesarias
@@ -73,7 +73,7 @@ st.text("")
 # Mostrar el título, fecha de actualización y porcentaje de viviendas georreferenciadas juntos
 st.markdown(f"""
     <h1 style='text-align: center;'>Viviendas con abordaje en búsqueda activa comunitaria por atención a brote de Fiebre Amarilla en Tolima</h1>
-    <p style='text-align: center; font-size: 14px;margin-bottom: 0px;'><b>Última fecha de actualización:</b> 15/02/2025</p>
+    <p style='text-align: center; font-size: 14px;margin-bottom: 0px;'><b>Última fecha de actualización:</b> 16/02/2025</p>
     <p style='text-align: center; font-size: 14px;'><b>Porcentaje de viviendas georreferenciadas:</b> {porcentaje_geo:.2f}% ({viviendas_geo} de {total_viviendas})</p>
 """, unsafe_allow_html=True)
 
