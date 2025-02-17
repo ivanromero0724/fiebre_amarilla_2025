7c10edba82500f036db047b15cd425cf86189122
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-02-17 00:41:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ st.text("")
 # Mostrar el título, fecha de actualización y porcentaje de viviendas georreferenciadas juntos
 st.markdown(f"""
     <h1 style='text-align: center;'>Viviendas con abordaje en búsqueda activa comunitaria por atención a brote de Fiebre Amarilla en Tolima</h1>
-    <p style='text-align: center; font-size: 14px;margin-bottom: -5px;'><b>Última fecha de actualización:</b> 16/02/2025</p>
+    <p style='text-align: center; font-size: 14px;margin-bottom: 0px;'><b>Última fecha de actualización:</b> 16/02/2025</p>
     <p style='text-align: center; font-size: 14px;margin-bottom: -5px;'><b>Porcentaje de viviendas georreferenciadas:</b> {porcentaje_geo:.2f}% ({viviendas_geo} de {total_viviendas})</p>
     <p style='text-align: center; font-size: 14px;'><b>Porcentaje de casos de Fiebre Amarilla georreferenciados:</b> {porcentaje_geo_fa:.2f}% ({casos_geo_fa} de {casos_geo_fa_total})</p>
 """, unsafe_allow_html=True)
