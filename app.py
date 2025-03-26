ff2cfe237a688c6178acc9a8079978ef5d438305
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-03-26 16:41:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ heat_data = [[lat, lon] for lat, lon in zip(fa_datos_2["LATITUD"], fa_datos_2["L
 # Crear la capa de calor
 heat_layer = HeatMap(heat_data, name="Mapa de Calor Casos de FA")
 # Crear un grupo de capas para el mapa de calor
-heat_group = folium.FeatureGroup(name="Mapa de calor casos confirmados de FA", show=False)  # show=False para no activar la capa
+heat_group = folium.FeatureGroup(name="Mapa de calor casos confirmados de FA", show=True)  # show=False para no activar la capa
 # Añadir la capa de calor al grupo
 heat_group.add_child(heat_layer)
 # Añadir el grupo al mapa
