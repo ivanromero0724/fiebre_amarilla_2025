853bc51ad0f868e06026411d4753cf7e827cc4b6
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-04-15 14:56:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -237,7 +237,7 @@ st.markdown("""
 
 st.components.v1.iframe(
     "https://lookerstudio.google.com/embed/reporting/cd506bb2-116f-4d30-8c1d-081dd48fbeb2/page/GV0EF",
-    width=1500,  # Ajusta según tu diseño
-    height=1500,  # Ajusta según tu diseño
+    width=2000,  # Ajusta según tu diseño
+    height=2000,  # Ajusta según tu diseño
     scrolling=True
 )
