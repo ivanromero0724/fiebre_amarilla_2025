fd59b4b4f5b68620c6ffa3afb0c7b889c4a5ba73
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-04-17 21:36:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,21 +15,6 @@ from folium.plugins import Draw
 
 st.set_page_config(layout="wide", page_title="Mapa Fiebre Amarilla Tolima", page_icon='🦟')
 
-if "autenticado" not in st.session_state:
-    st.session_state.autenticado = False
-
-if not st.session_state.autenticado:
-    clave = st.text_input("Clave de acceso", type="password")
-    if st.button("Entrar"):
-        if clave == st.secrets["password"]:
-            st.session_state.autenticado = True
-            st.experimental_rerun()
-        else:
-            st.error("Clave incorrecta")
-
-if st.session_state.autenticado:
-    st.success("Acceso concedido")
-
 # Reducir espacio superior con CSS
 st.markdown(
     """
