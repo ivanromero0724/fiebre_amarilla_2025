5b2564b3b1b82334b1e895354e83b2853482badb
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-04-22 17:00:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ else:
         total_viviendas = len(datos)
         viviendas_geo = len(datos_geo)
         porcentaje_geo = (viviendas_geo / total_viviendas) * 100
-        fa_datos = pd.read_excel("https://raw.githubusercontent.com/ivanromero0724/fiebre_amarilla_2025/main/FA_2025-04-22.xlsx",engine="openpyxl")
+        fa_datos = pd.read_excel("https://raw.githubusercontent.com/ivanromero0724/fiebre_amarilla_2025/main/FA_2025-04-22_FINAL.xlsx",engine="openpyxl")
         epizootias = pd.read_excel("https://raw.githubusercontent.com/ivanromero0724/fiebre_amarilla_2025/main/EPIZOOTIAS_FINAL_2.xlsx",engine="openpyxl")
         # Contar los valores no NAN en la columna LATITUD
         casos_geo_fa = fa_datos['LATITUD'].notna().sum()
