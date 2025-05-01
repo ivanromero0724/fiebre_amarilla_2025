754e86859524919c621708ae79c65e3d8069bcbb
ivanromero0724 <158233460+ivanromero0724@users.noreply.github.com>
2025-04-30 19:18:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ else:
         viviendas_geo = len(datos_geo)
         porcentaje_geo = (viviendas_geo / total_viviendas) * 100
         fa_datos = pd.read_excel("https://raw.githubusercontent.com/ivanromero0724/fiebre_amarilla_2025/main/FA_2025-04-28.xlsx",engine="openpyxl")
-        epizootias = pd.read_excel("https://raw.githubusercontent.com/ivanromero0724/fiebre_amarilla_2025/main/EPIZOOTIAS_final_2.xlsx",engine="openpyxl")
+        epizootias = pd.read_excel("https://raw.githubusercontent.com/ivanromero0724/fiebre_amarilla_2025/main/EPIZOOTIAS_final_3.xlsx",engine="openpyxl")
         # Contar los valores no NAN en la columna LATITUD
         casos_geo_fa = fa_datos['LATITUD'].notna().sum()
         casos_geo_fa_total = len(fa_datos)
@@ -122,7 +122,7 @@ else:
         # Mostrar el título, fecha de actualización y porcentaje de viviendas georreferenciadas juntos
         st.markdown(f"""
             <h1 style='text-align: center;'>Monitoreo del Brote de Fiebre Amarilla en Colombia (2024-2025)</h1>
-            <p style='text-align: center; font-size: 14px;margin-bottom: 0px;'><b>Última fecha de actualización:</b> 28/04/2025</p>
+            <p style='text-align: center; font-size: 14px;margin-bottom: 0px;'><b>Última fecha de actualización:</b> 30/04/2025</p>
             <p style='text-align: center; font-size: 14px;margin-bottom: 0px;'><b>Porcentaje de viviendas georreferenciadas:</b> {porcentaje_geo:.2f}% ({viviendas_geo} de {total_viviendas})</p>
             <p style='text-align: center; font-size: 14px;'><b>Porcentaje de casos de Fiebre Amarilla georreferenciados:</b> {porcentaje_geo_fa:.2f}% ({casos_geo_fa} de {casos_geo_fa_total})</p>
         """, unsafe_allow_html=True)
